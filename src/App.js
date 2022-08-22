bd406c7e24b2490e46e008b41663f71e446220c9
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-22 13:15:31 -0700

    add initial navbar set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { TopNav } from "./components";
 import Home from './pages/HomePage/Home';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <>
       <Router>
+        <TopNav />
         <Routes>
           <Route path='/' exact element={<Home />} />
         </Routes>
---
+++ src/components/TopNavbar/TopNav.js
@@ -0,0 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function TopNav() {
+  return (
+    <Navbar collapseOnSelect expand="md" styled={{ opacity: '0' }}>
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button>Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopNav;
