fa121dc371561079f609f4a1671af98b54bc5a8e
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-22 18:09:41 -0700

    begin adding card link section

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,10 +1,12 @@
 import '../../App.css';
+import CardLinkSection from '../../components/CardLinkSection/CardLinkSection';
 import { PhotoCarousel } from '../../components/index';
 
 const Home = () => {
   return (
     <>
       <PhotoCarousel />
+      <CardLinkSection />
     </>
   )
 }
