8ab2c7ceed5292ebbc21e94849bf215d5441e22c
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-24 15:26:02 -0700

    add scroll effect

--- src/components/AdvertLinkSection/AdvertLinkSection.js
+++ src/components/AdvertLinkSection/AdvertLinkSection.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Card } from 'react-bootstrap';
 import { AdvertSection, AdvertSectionHeading, Img, ImgWrapper, BannerText, BannerTextWrapper } from './AdvertLinkSection.elements';
 import photo from '../../images/couch-banner.webp';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const AdvertLinkSection = ({advertAlt, advertHeading, bannerText, advertImg}) => {
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
+
   return(
     <>
-      <AdvertSection>
-        <Container>
-          <AdvertSectionHeading>{advertHeading}</AdvertSectionHeading>
-          <Link to="/designs">
-            <ImgWrapper>
-              <Card>
-                <Img src={photo} alt={advertAlt}/>
-                <BannerTextWrapper>
-                  <BannerText>{bannerText}</BannerText>
-                </BannerTextWrapper>
-              </Card>
-            </ImgWrapper>
-          </Link>
-        </Container>
-      </AdvertSection>
+        <AdvertSection>
+          <Container>
+            <AdvertSectionHeading>{advertHeading}</AdvertSectionHeading>
+            <Link to="/designs">
+              <div data-aos='fade-up'>
+                <ImgWrapper>
+                  <Card>
+                    <Img src={photo} alt={advertAlt}/>
+                    <BannerTextWrapper>
+                      <BannerText>{bannerText}</BannerText>
+                    </BannerTextWrapper>
+                  </Card>
+                </ImgWrapper>
+              </div>
+            </Link>
+          </Container>
+        </AdvertSection>
     </>
   )
 };
